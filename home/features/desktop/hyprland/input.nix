53e3563bb3d9de2cf5c50157eeec1fccdb228c91
Dmitriy Dmitriev <qdimka91@gmail.com>
2025-05-24 22:33:15 +0300

    Fix kbd

--- home/features/desktop/hyprland/input.nix
+++ home/features/desktop/hyprland/input.nix
@@ -1,7 +1,7 @@
 {
   wayland.windowManager.hyprland.settings.input = {
     follow_mouse = 1;
-    kb_layout = "en,ru";
+    kb_layout = "us,ru";
     kb_variant = "nodeadkeys";
     kb_options = "grp:alt_shift_toggle";
     sensitivity = 0.4;
