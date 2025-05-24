dc5907a0eba2b02d89c60b1d7726fe8dbd33ace5
Dmitriy Dmitriev <qdimka91@gmail.com>
2025-05-24 13:08:29 +0300

    Add bluetooth

---
+++ hosts/station/bluetooth.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }: 
+{
+  environment.systemPackages = with pkgs; [ blueman ];
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
+  };
+  hardware.pulseaudio = {
+    enable = true;
+    package = pkgs.pulseaudioFull;
+  };
+  services.blueman.enable = true;
+
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = [ "network.target" "sound.target" ];
+    wantedBy = [ "default.target" ];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  };
+}
--- hosts/station/configuration.nix
+++ hosts/station/configuration.nix
@@ -7,6 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
+      ./bluetooth.nix
       ./hardware-configuration.nix
     ];
 
